c6cf53be181922b3f22b6c2cf6b0e6a5b966bcfb
Bánki László <bankilac@gmail.com>
2025-03-24 17:55:00 +0100

    Frontend integration with vod management service

--- vod management service/main.py
+++ vod management service/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from database import init_db
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 
 app = FastAPI()
+
+# CORS konfiguráció
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # Frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Engedélyezett metódusok
+    allow_headers=["*"],  # Engedélyezett headerek
+)
 
 # API útvonalak hozzáadása
 app.include_router(router)
