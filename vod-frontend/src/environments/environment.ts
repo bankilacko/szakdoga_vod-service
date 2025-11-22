97d7b197d99f502b7824539c722ee3fd7a77accb
bankilacko <bankilac@gmail.com>
2025-11-22 14:30:59 +0100

    frontend env ip addresses

--- vod-frontend/src/environments/environment.ts
+++ vod-frontend/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   // API Gateway URL - backend service-ekhez
-  // apiBaseUrl: 'http://152.66.245.139:22291'
-  apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: 'http://152.66.245.139:22291',
+  // apiBaseUrl: 'http://localhost:5000',
   
   // VOD Server URL - videó streameléshez
-  // vodServerUrl: 'http://152.66.245.139:22292/vod'
-  vodServerUrl: 'http://localhost:7000/vod',
+  vodServerUrl: 'http://152.66.245.139:22292/vod',
+  //vodServerUrl: 'http://localhost:7000/vod',
 };
 
